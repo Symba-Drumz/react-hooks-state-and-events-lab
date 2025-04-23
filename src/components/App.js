49e03b440b19a77e9a729b46496b58c58a41d2e2
alfazachmmosi@TimeMachine <zalfa3890@gmail.com>
2025-04-23 03:18:11 +0300

    feat: modify code for Apps.js, Items.js and ShoppingList.js to toggle button do what is required

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
+  // state variable for dark mode toggle
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  // className based on dark mode state
+  const appClass = isDarkMode ? "App dark" : "App light";
+
+  // event handler to toggle dark mode
+  function handleDarkModeClick() {
+    setIsDarkMode((isDark) => !isDark);
+  }
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleDarkModeClick}>Dark Mode</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
